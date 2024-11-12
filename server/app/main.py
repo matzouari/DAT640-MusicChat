ea0069f359a23f195b77a2283c09d6e27c5e1dbb
matzouari <146164559+matzouari@users.noreply.github.com>
2024-11-12 16:02:01 +0100

    removed frontend support

--- server/app/main.py
+++ server/app/main.py
@@ -24,28 +24,8 @@ def connect_to_db():
         print(f"Error: {err}")
         return None
 
-
-# Initialize global database connection
-conn = connect_to_db()  # Call the function and store the connection globally
-
 platform = musicPlatform(MusicAgent)
 app = platform.app
-
-@app.route('/get_playlist', methods=['GET'])
-def get_playlist():
-    """API endpoint to fetch playlist for the front end."""
-    try:
-        if conn and conn.is_connected():
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute("SELECT track_name, artist_name, album_name, genre, popularity FROM playlist")
-            songs = cursor.fetchall()
-            cursor.close()
-            return jsonify(songs)
-        else:
-            return jsonify({"error": "Database connection is not available"}), 500
-    except Error as err:
-        print(f"Error: {err}")
-        return jsonify({"error": "Failed to fetch playlist"}), 500
 
 
 if __name__ == '__main__':
